76cef42776b155d4cf312e1e7ddfa824af3999d7
Ndaru Pratiwi <ui.tiwi@gmail.com>
2021-06-13 23:54:14 +0700

    Rest API

---
+++ app/src/main/java/com/example/restci_wpf/Delete.java
@@ -0,0 +1,89 @@
+package com.example.restci_wpf;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.os.Bundle;
+import java.util.HashMap;
+import java.util.Map;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.example.restci_wpf.Util.ServerApi;
+import com.example.restci_wpf.Util.AppController;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+public class Delete extends AppCompatActivity {
+    EditText deleteID ;
+    Button btnDelete;
+    ProgressDialog pd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete);
+
+        deleteID = (EditText) findViewById(R.id.username_param);
+        btnDelete = (Button) findViewById(R.id.btn_delete);
+        pd = new ProgressDialog(Delete.this);
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteData();
+            }
+        });
+    }
+
+    private void deleteData()
+    {
+        pd.setMessage("Delete Data ...");
+        pd.setCancelable(false);
+        pd.show();
+
+        StringRequest delReq = new StringRequest(Request.Method.POST,ServerApi.URL_DELETE,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        pd.cancel();
+                        Log.d("volley","response : " + response.toString());
+                        try {
+                            JSONObject res = new JSONObject(response);
+                            Toast.makeText(Delete.this,"pesan : " +res.getString("message"), Toast.LENGTH_SHORT).show();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        startActivity(new Intent(Delete.this,MainActivity.class));
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        pd.cancel();
+                        Log.d("volley", "error : " + error.getMessage());
+                        Toast.makeText(Delete.this, "pesan : gagal menghapus data", Toast.LENGTH_SHORT).show();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> map = new HashMap<>();
+                map.put("username",deleteID.getText().toString());
+                return map;
+            }
+        };
+
+        AppController.getInstance().addToRequestQueue(delReq);
+    }
+
+}
